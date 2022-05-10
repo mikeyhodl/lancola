12381f50a67dea03344d1ea63c1c565bdf8956fc
Mike Owino <mike@lancolatech.co.ke>
2022-05-10 12:26:23 +0300

    ....

--- alert.py
+++ alert.py
@@ -2,8 +2,10 @@ import requests
 
 url = "https://api.telegram.org/bot5268748619%3AAAGHo1QI6eNRp_Vw3dor28XtwIqJocmJm10/sendPhoto"
 
+
+
 payload = {
-    "photo": "https://s3.tradingview.com/snapshots/r/RBVk9Yuv.png",
+    "photo": "https://pbs.twimg.com/media/FSATBDHXMAA6yhc?format=jpg&name=4096x4096",
     "caption": "BNBUSDT | BINANCE",
     "disable_notification": False,
     "reply_to_message_id": None,
